4d56e6342fa2880a92b7d81bd01f55842e88390d
JC <cjoaquinangel@gmail.com>
2024-07-02 13:19:33 -0600

    created base api

--- src/main/java/easyBroker/App.java
+++ src/main/java/easyBroker/App.java
@@ -1,6 +1,10 @@
 package easyBroker;
 import java.io.IOException;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -11,14 +15,24 @@ class App{
         OkHttpClient client=new OkHttpClient();
 
         Request request=new Request.Builder()
-        .url("https://reqres.in/api/users?page=2")
+        .url("https://api.stagingeb.com/v1/properties?page=1&limit=2")
+        .get()
+        .addHeader("X-Authorization", "l7u502p8v46ba3ppgvj5y2aad50lb9")
+        .addHeader("accept", "application/json")
         .build();
 
         try(Response response = client.newCall(request).execute()){
             if(!response.isSuccessful()){
                 throw new IOException("Unexteped code "+response);
             }
-            System.out.println(response.body().string());
+            String jsonResponse = response.body().string();
+            Gson gson=new Gson();
+            JsonObject jsonObject=gson.fromJson(jsonResponse, JsonObject.class);
+            JsonArray contentArray=jsonObject.get("content").getAsJsonArray();
+            for(int i=0;i<contentArray.size();i++)
+            {
+                System.out.println(contentArray.get(i).getAsJsonObject().get("title"));
+            }
             
         }
     }
